c78abdc207e4abbd924e719e825104882995b752
Dean Ma <mrdingma@gmail.com>
2020-02-12 17:59:42 -0800

    refactor server for eventual server side rendering

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,59 @@
 const models = require('../models');
 
 module.exports = {
+  reviews: {
+    post: async (req, res, next) => {
+      try {
+        const { hotelId } = req.params;
+        const data = req.body;
+        const reviews = await models.reviews.post(hotelId, data);
+        return res.sendStatus(201);
+      } catch (err) {
+        return next(err);
+      }
+    },
+    getAll: async (req, res, next) => {
+      try {
+        const { hotelId } = req.params;
+        const reviews = await models.reviews.getAll(hotelId);
+        return res.json(reviews);
+      } catch (err) {
+        return next(err);
+      }
+    },
+    get: async (req, res, next) => {
+      try {
+        const { hotelId } = req.params;
+        const { reviewId } = req.params;
 
+        const review = await models.reviews.get(hotelId, reviewId);
+        return res.json(review);
+      } catch (err) {
+        return next(err);
+      }
+    },
+    remove: async (req, res, next) => {
+      try {
+        const { hotelId } = req.params;
+        const { reviewId } = req.params;
 
+        const reviews = await models.reviews.remove(hotelId, reviewId);
+        return res.sendStatus(200);
+      } catch (err) {
+        return next(err);
+      }
+    },
+    update: async (req, res, next) => {
+      try {
+        const { hotelId } = req.params;
+        const { reviewId } = req.params;
+        const data = req.body;
 
-
-
-}
+        const reviews = await models.reviews.update(hotelId, reviewId, data);
+        return res.sendStatus(200);
+      } catch (err) {
+        return next(err);
+      }
+    },
+  },
+};
