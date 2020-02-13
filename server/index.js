d578f0246b0ec1c5c70bbe6115c75cb75c8585a5
Dean Ma <mrdingma@gmail.com>
2020-02-12 16:44:54 -0800

    create express CRUD routes

--- server/index.js
+++ server/index.js
@@ -4,20 +4,33 @@ const app = express();
 const port = 3001;
 const db = require("./db.js");
 const compression = require("compression");
+const controllers = require('./controllers');
 
 app.use("/:id", express.static(path.join(__dirname, "../client/dist")));
 app.use(express.json());
 app.use(compression());
 
-app.get("/reviews/hotels/:hotelId", (req, res) => {
-  db.getReviewsbyID(req.params.hotelId, (err, results) => {
-    if (err) {
-      console.log(err);
-      res.sendStatus(500);
-    } else {
-      res.send(results);
-    }
-  });
-});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.post('/reviews/hotels/:hotelId', controllers.reviews.post);
+
+// GET all reviews
+app.get('/reviews/hotels/:hotelId', controllers.reviews.getAll);
+
+// GET one review
+app.get('/reviews/hotels/:hotelId/:reviewId', controllers.reviews.get);
+
+// DELETE one review
+app.delete('/reviews/hotels/:hotelId/:reviewId', controllers.reviews.get);
+
+// app.get("/reviews/hotels/:hotelId", (req, res) => {
+//   db.getReviewsbyID(req.params.hotelId, (err, results) => {
+//     if (err) {
+//       console.log(err);
+//       res.sendStatus(500);
+//     } else {
+//       res.send(results);
+//     }
+//   });
+// });
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
---
+++ server/models/index.js
@@ -0,0 +1,3 @@
+const db_cassandra = require('../db_cassandra');
+const db_postgres = require('../db_postgres');
+
