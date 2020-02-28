e8ea9f42edac591ff8c9eb96dc876024631b0bae
Dean Ma <mrdingma@gmail.com>
2020-02-28 15:56:24 -0800

    add new contact points

--- server/db/cassandra/index.js
+++ server/db/cassandra/index.js
@@ -1,6 +1,14 @@
 const cassandra = require('cassandra-driver');
 
+// DB instances
 module.exports = new cassandra.Client({
   contactPoints: ['18.144.37.253', '54.183.191.164', '13.57.186.28', '54.219.161.236', '13.56.228.55'],
   localDataCenter: 'us-west',
 });
+
+
+// LOCAL
+// module.exports = new cassandra.Client({
+//   contactPoints: ['127.0.0.1'],
+//   localDataCenter: 'datacenter1',
+// });
